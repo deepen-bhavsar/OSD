f414f50251594a4e275968d80e5ef7ae567d55db
prasang007 <prasang00@gmail.com>
2018-08-01 17:57:40 +0530

    initialize

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#knob").click(function(){
+        $("#upper").animate({top:"-=50%"},1500,"linear");
+        $("#lower").animate({top:"+=50%"},1500,"linear");
+        $("#knob").hide();
+    });
+});
