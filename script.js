a1e8086ce4fedcd094f01c7f0dd25f4876bff204
prasang007 <prasang00@gmail.com>
2018-08-01 22:21:38 +0530

    updates

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ $(document).ready(function(){
         $("#upper").animate({top:"-=50%"},1500,"linear");
         $("#lower").animate({top:"+=50%"},1500,"linear");
         $("#knob").hide();
+        $("#shelf-container").show();
     });
 });
