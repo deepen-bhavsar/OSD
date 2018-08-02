e85f16fc6a8fd1da142355f0fe3e530b109aa6cf
Nishchay Jain <nishchayjain.it20@jecrc.ac.in>
2018-08-02 16:53:22 +0530

    Add files via upload

---
+++ nishchay2.js
@@ -0,0 +1,7 @@
+document.querySelector(".card-flip").classList.toggle("flip");
+Holder.addTheme('gray', {
+  bg: '#777',
+  fg: 'rgba(255,255,255,.75)',
+  font: 'Helvetica',
+  fontweight: 'normal'
+});
